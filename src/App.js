99e610a5031e0d299359c4a14211b533a4a80761
Balthasar Hofer <balthasar.hofer@swissdrg.org>
2020-05-13 21:11:33 +0200

    dynamically configure protocoll

--- src/App.js
+++ src/App.js
@@ -8,19 +8,24 @@ import socketioClient from "socket.io-client";
 
 export const API_URL = "http://localhost:4001";
 
-/**
- * either use 'http' or 'socket'
- */
-export const PROTOCOL = 'socket';
 
 export default class App extends React.Component {
   state = {
-    words: []
+    words: [],
+    protocol: 'socket' // either 'socket' or 'http'
   };
 
   componentDidMount() {
-    switch (PROTOCOL) {
+    this.configureProtocol();
+  }
+
+  configureProtocol() {
+    switch (this.state.protocol) {
       case 'http':
+        if (this.socket) {
+          this.socket.disconnect();
+          this.socket = undefined;
+        }
         this.loadWords();
         break;
       case 'socket':
@@ -36,11 +41,15 @@ export default class App extends React.Component {
     if (word.word.length <= 1 || word.word.length > 16) {
       return;
     }
-    switch (PROTOCOL) {
+    switch (this.state.protocol) {
       case 'http':
-        axios.post(`${API_URL}/words`, word).then(() => {
-          this.loadWords();
-        });
+
+        // const xhr = new XMLHttpRequest();
+        // xhr.open("POST", `${API_URL}/words`, true);
+        // xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        // xhr.setRequestHeader("Accept-Encoding", "identity");
+        // xhr.send(JSON.stringify(word));
+        axios.post(`${API_URL}/words`, word);
         break;
       case 'socket':
         this.socket.emit("add_word", word);
@@ -50,35 +59,82 @@ export default class App extends React.Component {
     }
   };
 
+  clearWords() {
+    switch (this.state.protocol) {
+      case 'http':
+        axios
+          .post(`${API_URL}/clear`)
+          .then(() => {
+            this.loadWords()
+          });
+        break;
+      case 'socket':
+        this.socket.emit("clear");
+        break;
+      default:
+        break;
+    }
+  }
+
+  toggleProtocol() {
+    if (this.state.protocol === 'http') {
+      this.setState({ protocol: 'socket' });
+    } else {
+      this.setState({ protocol: 'http' });
+    }
+  }
+
   loadWords() {
-    axios
-      .get(`${API_URL}/words`)
-      .then(({ data }) => {
-        this.setState({ words: data });
-      })
-      .catch(error => {
-        console.log(error);
-      });
+    switch (this.state.protocol) {
+      case 'http':
+        axios
+          .get(`${API_URL}/words`)
+          .then(({ data }) => {
+            this.setState({ words: data });
+          })
+          .catch(error => {
+            console.log(error);
+          });
+        break;
+      case 'socket':
+        this.socket.emit("get_words");
+        break;
+      default:
+        break;
+    }
   }
 
 
   render() {
     return (
       <div className="App">
-        <h1>Wordcloud {PROTOCOL.toUpperCase()}
-          {
-            /* only show the refresh button when http-protocol is used */
-            PROTOCOL === 'http' &&
+        <h1>Wordcloud {this.state.protocol.toUpperCase()}</h1>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <div style={{ display: 'flex', alignItems: 'baseline' }}>
+            <h4 style={{ marginRight: '1em' }}>Refresh</h4>
             <button title="Refresh" onClick={() => this.loadWords()}>↻</button>
-          }
-        </h1>
+          </div>
+          <div style={{ display: 'flex', alignItems: 'baseline' }}>
+            <h4 style={{ marginRight: '1em' }}>Protocol</h4>
+            <button
+              title="HTTP"
+              className={this.state.protocol === 'http' ? 'active' : ''}
+              onClick={() => this.setState({ protocol: 'http' })}>
+              http
+            </button>
+            <button
+              title="Socket"
+              className={this.state.protocol === 'socket' ? 'active' : ''}
+              onClick={() => this.setState({ protocol: 'socket' })}>
+              webSockets
+            </button>
+          </div>
+        </div>
+        <div style={{ height: '2em' }}></div>
+
         <Wordcloud words={this.state.words} />
         <WordForm addWord={this.addWord} />
-        {
-          /* only show the refresh button when http-protocol is used */
-          PROTOCOL === 'socket' &&
-          <button title="Clear" onClick={() => this.socket.emit('clear')}>🗑️</button>
-        }
+        <button title="Clear" onClick={() => this.clearWords()}>🗑️</button>
         <Eventlog words={this.state.words} />
       </div>
     );
